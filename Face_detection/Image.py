3db7a752dd6f876528ba0e7288bc7f73d19eba36
SurabhiP13 <64202735+SurabhiP13@users.noreply.github.com>
2021-07-09 19:43:43 +0530

    Update Image.py

--- Face_detection/Image.py
+++ Face_detection/Image.py
@@ -4,14 +4,17 @@ import numpy as np
 img=cv.imread('facee.jpg') #image path
 cv.imshow('image', img)
 
+#converts image to grayscale
 gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 haar_cascade=cv.CascadeClassifier('haar_face.xml')
 
+#compares the haar template with out image
 face_rect=haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
 print(f'Number of people found: {len(face_rect)}')
 
+#makes a rectangle over the detected face
 for(x, y, w, h) in face_rect:
     cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 
